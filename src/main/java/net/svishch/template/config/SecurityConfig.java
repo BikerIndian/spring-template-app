d5b466e4523dcec144663e9dcef1979092c29c04
v.svich <5693031@gmail.com>
2024-01-12 09:53:05 +0300

    Start

---
+++ src/main/java/net/svishch/template/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package net.svishch.template.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig   {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                //.antMatchers("/", "/home").permitAll()
+                .antMatchers("/skin/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin() //стандартная форма
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("111"))
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
+
+    // PasswordEncoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
